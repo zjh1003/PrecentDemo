ec72ec2a0d8a57d3b9bbd0961d1ecc44e5d017bb
unknown <1392096101@qq.com>
2017-06-21 11:13:46 +0800

    Signed-off-by: unknown <1392096101@qq.com>

---
+++ app/src/main/java/com/example/yls/precentdemo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.yls.precentdemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        mButton = (Button) findViewById(R.id.btn);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,PrecentFramActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/yls/precentdemo/PrecentFramActivity.java
@@ -0,0 +1,18 @@
+package com.example.yls.precentdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yls on 2017/3/27.
+ */
+
+public class PrecentFramActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.precentfram_activity);
+    }
+}
